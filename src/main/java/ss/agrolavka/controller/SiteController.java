0c4388024b8282e9ebcd621b928f5c359183b3a2
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-02-14 13:35:25 +0300

    Initial commit.

---
+++ src/main/java/ss/agrolavka/controller/SiteController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss.agrolavka.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author alex
+ */
+@Controller
+public class SiteController {
+    @RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("name", "Alex");
+        return "welcome";
+    }
+}
