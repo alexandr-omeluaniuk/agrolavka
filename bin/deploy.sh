d0f1b4d1ecb57a6a228f3aa4dd341e1ac69a1bc7
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-02-24 21:00:30 +0300

    Deploy.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 /home/alex/netbeans-12.0/netbeans/java/maven/bin/mvn clean install
 scp -i /media/veracrypt1/agrolavka/id_rsa_agrolavka target/agrolavka.war agrolavka@157.90.160.237:/home/agrolavka
+ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "chmod 500 agrolavka.war"
+#ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "service agrolavka restart"
