62a048bcdfd0f773fbd41590d47fc632ca6fb103
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-02-25 22:19:23 +0300

    Improvements.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /home/alex/netbeans-12.0/netbeans/java/maven/bin/mvn clean install
+ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "rm agrolavka.war"
 scp -i /media/veracrypt1/agrolavka/id_rsa_agrolavka target/agrolavka.war agrolavka@157.90.160.237:/home/agrolavka
 ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "chmod 500 agrolavka.war"
 ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "sudo service agrolavka restart"
