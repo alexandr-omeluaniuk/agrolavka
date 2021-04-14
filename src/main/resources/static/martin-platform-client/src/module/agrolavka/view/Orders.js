51c56be77db6fc74f4f4ffa65f924549982da06f
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-04-14 20:29:52 +0300

    Order.

--- src/main/resources/static/martin-platform-client/src/module/agrolavka/view/Orders.js
+++ src/main/resources/static/martin-platform-client/src/module/agrolavka/view/Orders.js
@@ -6,9 +6,8 @@
 import React, { useEffect } from 'react';
 import DataService from '../../../service/DataService';
 import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@material-ui/core/styles';
-import { TableConfig, TableColumn, FormConfig, FormField, Validator, ALIGN_RIGHT, ApiURL } from '../../../util/model/TableConfig';
-import { TYPES, VALIDATORS } from '../../../service/DataTypeService';
+import { TableConfig, TableColumn, FormConfig, FormField, ALIGN_RIGHT, ApiURL } from '../../../util/model/TableConfig';
+import { TYPES } from '../../../service/DataTypeService';
 import DataTable from '../../../component/datatable/DataTable';
 import Link from '@material-ui/core/Link';
 import Icon from '@material-ui/core/Icon';
@@ -19,12 +18,7 @@ import { requestFirebaseToken } from '../../../conf/firebase';
 
 let dataService = new DataService();
 
-const useStyles = makeStyles(theme => ({
-    
-}));
-
 function Orders() {
-    const classes = useStyles();
     const { t } = useTranslation();
     const [tableConfig, setTableConfig] = React.useState(null);
     // ------------------------------------------------------- METHODS --------------------------------------------------------------------
@@ -66,7 +60,6 @@ function Orders() {
                 '/agrolavka/protected/order'
         );
         const newTableConfig = new TableConfig(t('m_agrolavka:agrolavka.orders'), apiUrl, [
-            //new TableColumn('name', t('m_agrolavka:products.product_name')).setSortable(),
             new TableColumn('id', t('m_agrolavka:orders.order_number'), (row) => {
                 let num = row.id.toString();
                 while (num.length < 5) num = "0" + num;
@@ -81,15 +74,14 @@ function Orders() {
                     return `${adr.postcode ? adr.postcode + ' ' : ''}${adr.city}, ${adr.street} д.${adr.house} ${adr.flat ? 'кв.' + adr.flat : ''}`;
                 }
                 return 'Самовывоз';
-            })
-//            new TableColumn('code', t('m_agrolavka:products.product_code')).setSortable().width('160px').alignment(ALIGN_RIGHT),
-//            new TableColumn('article', t('m_agrolavka:products.product_article')).setSortable().width('160px').alignment(ALIGN_RIGHT),
-//            new TableColumn('buyPrice', t('m_agrolavka:products.product_buy_price'), (row) => {
-//                return parseFloat(row.buyPrice).toFixed(2);
-//            }).setSortable().width('100px').alignment(ALIGN_RIGHT),
-//            new TableColumn('price', t('m_agrolavka:products.product_price'), (row) => {
-//                return parseFloat(row.price).toFixed(2);
-//            }).setSortable().width('100px').alignment(ALIGN_RIGHT)
+            }),
+            new TableColumn('total', t('m_agrolavka:orders.order_total'), (row) => {
+                let total = 0;
+                row.positions.forEach(pos => {
+                    total += pos.quantity * pos.price;
+                });
+                return parseFloat(total).toFixed(2) + ' BYN';
+            }).width('170px').alignment(ALIGN_RIGHT)
         ], new FormConfig([
             new FormField('id', TYPES.ID).hide()
         ])).setElevation(1).setToolbarActionsBefore(toolbarBefore());
