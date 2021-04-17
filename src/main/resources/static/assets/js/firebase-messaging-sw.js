5e1c6ad504e1d56d6d73331c581bca427c4b7d9a
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-04-17 15:48:55 +0300

    Firebase.

--- src/main/resources/static/assets/js/firebase-messaging-sw.js
+++ src/main/resources/static/assets/js/firebase-messaging-sw.js
@@ -5,9 +5,9 @@ self.addEventListener("push", (event) => {
 async function onPush(event) {
     const push = event.data.json();
     console.log("push received", push);
-    const {notification = {}, data = {}} = {...push};
-    notification.actions = JSON.parse(data['gcm.notification.actions'] ? data['gcm.notification.actions'] : []);
-    notification.data = data;
+    const {notification = {}/*, data*/ } = {...push};
+//    notification.actions = JSON.parse(data && data['gcm.notification.actions'] ? data['gcm.notification.actions'] : []);
+//    notification.data = data ? data : {};
     notification.badge = 'https://agrolavka.by/admin/logo24.png';
     notification.icon = 'https://agrolavka.by/admin/logo64.png';
     await self.registration.showNotification(notification.title, notification);
