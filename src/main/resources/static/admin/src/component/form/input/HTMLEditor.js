57ea795dfe284d546174417a8a217d2cf57f24a0
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-05-29 17:39:22 +0300

    HTML editor.

--- src/main/resources/static/admin/src/component/form/input/HTMLEditor.js
+++ src/main/resources/static/admin/src/component/form/input/HTMLEditor.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
@@ -20,10 +20,15 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(1)
     },
     row: {
+        flex: 1,
+        display: 'flex'
+    },
+    colLeft: {
         flex: 1
     },
-    toolbar: {
-        marginBottom: theme.spacing(1)
+    colRight: {
+        flex: 1,
+        padding: theme.spacing(2)
     },
     preview: {
         
@@ -34,18 +39,39 @@ function HTMLEditor (props) {
     const classes = useStyles();
     const { label, name, required, helperText, value, onChangeFieldValue, labelWidth, rows } = props;
     const [showSourceCode, setShowSourceCode] = React.useState(true);
+    const [shadowRoot, setShadowRoot] = React.useState(null);
+    const shadowRef = React.useRef(null);
+    // ------------------------------------------- HOOKS ----------------------------------------------------------------------------------
+    useEffect(() => {
+        if (shadowRef && shadowRef.current && shadowRoot === null) {
+            let shadow = shadowRef.current.attachShadow({mode: 'open'});
+            setShadowRoot(shadow);
+        }
+    }, [shadowRef, shadowRoot]);
+    useEffect(() => {
+        console.log(shadowRoot);
+        if (shadowRoot) {
+            shadowRoot.innerHTML = `<body>${value !== null && value !== undefined ? value : ''}</body>`;
+        }
+    }, [value, shadowRoot]);
+    // ------------------------------------------- RENDERING ------------------------------------------------------------------------------
     return (
             <Paper className={classes.container} elevation={0}>
                 <HTMLEditorToolbar showSourceCode={showSourceCode} setShowSourceCode={setShowSourceCode}/>
                 {showSourceCode ? (
                     <div className={classes.row}>
-                        <FormControl variant={'outlined'} fullWidth required={required}>
-                            <InputLabel>{label}</InputLabel>
-                            <OutlinedInput value={value ? value : ''} multiline={true} rows={rows} onChange={(e) => {
-                                onChangeFieldValue(name, e.target.value);
-                            }} labelWidth={labelWidth}/>
-                            {helperText ? <FormHelperText variant={'outlined'} error={true}>{helperText}</FormHelperText> : null}
-                        </FormControl>
+                        <div className={classes.colLeft}>
+                            <FormControl variant={'outlined'} fullWidth required={required}>
+                                <InputLabel>{label}</InputLabel>
+                                <OutlinedInput value={value ? value : ''} multiline={true} rows={rows} onChange={(e) => {
+                                    onChangeFieldValue(name, e.target.value);
+                                }} labelWidth={labelWidth} />
+                                {helperText ? <FormHelperText variant={'outlined'} error={true}>{helperText}</FormHelperText> : null}
+                            </FormControl>
+                        </div>
+                        <div className={classes.colRight} ref={shadowRef}>
+    
+                        </div>
                     </div>
                 ) : (
                     <div className={classes.preview} dangerouslySetInnerHTML={{ __html: value }}>
