bcc17cc3f01e13d2a3084f3753431dc63ef31a49
alex <alex@sonya>
2021-10-03 17:55:30 +0300

    Scripts.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+KEY=/home/alex/secure/agrolavka/id_rsa_agrolavka
+MAVEN_HOME=/home/alex/work/soft/netbeans/java/maven/bin/mvn
 cd src/main/resources/static/admin
 npm run build
 cd ../../../../../
-/home/alex/netbeans-12.0/netbeans/java/maven/bin/mvn clean install
-ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "rm agrolavka.war"
-scp -i /media/veracrypt1/agrolavka/id_rsa_agrolavka target/agrolavka.war agrolavka@157.90.160.237:/home/agrolavka
-ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "chmod 500 agrolavka.war"
-ssh -i /media/veracrypt1/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237 "sudo service agrolavka restart"
+${MAVEN_HOME} clean install
+ssh -i ${KEY} agrolavka@157.90.160.237 "rm agrolavka.war"
+scp -i ${KEY} target/agrolavka.war agrolavka@157.90.160.237:/home/agrolavka
+ssh -i ${KEY} agrolavka@157.90.160.237 "chmod 500 agrolavka.war"
+ssh -i ${KEY} agrolavka@157.90.160.237 "sudo service agrolavka restart"
