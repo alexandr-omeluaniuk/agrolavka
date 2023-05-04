e134e9e7ff6f2dba4c8db670d6f7ca3793d4b75e
alex <alex@sonya>
2023-05-04 12:19:34 +0300

    clear all

--- bin/ssh_agrolavka.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh -i /home/alex/secure/agrolavka/id_rsa_agrolavka agrolavka@157.90.160.237
--- bin/transfer_ssl_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-KEY=/home/alex/secure/agrolavka/id_rsa_agrolavka
-scp -i ${KEY} /home/alex/secure/agrolavka/agrolavka.by.zip agrolavka@157.90.160.237:/home/agrolavka/ssl
