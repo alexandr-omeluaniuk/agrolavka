75f91663e503d05cf78346afa54d936befd609a2
alex <alex@sonya>
2023-05-09 22:17:34 +0300

    SMTP email client

--- module/lib/notification/notification-email-smtp/src/main/java/ss/martin/notification/email/smtp/SmtpSender.java
+++ module/lib/notification/notification-email-smtp/src/main/java/ss/martin/notification/email/smtp/SmtpSender.java
@@ -1,7 +1,12 @@
 package ss.martin.notification.email.smtp;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import ss.martin.notification.email.api.EmailService;
 import ss.martin.notification.email.api.model.EmailRequest;
@@ -14,11 +19,19 @@ import ss.martin.notification.email.api.model.EmailRequest;
 class SmtpSender implements EmailService {
     
     private static final Logger LOG = LoggerFactory.getLogger(SmtpSender.class);
+    
+    @Autowired
+    private JavaMailSender emailSender;
 
     @Override
     public void sendEmail(final EmailRequest emailRequest) throws Exception {
         LOG.debug(emailRequest.toString());
-        
+        final var message = new SimpleMailMessage(); 
+        message.setFrom(emailRequest.sender().email());
+        message.setTo(Stream.of(emailRequest.recipients()).map(recipient -> recipient.email()).collect(Collectors.toList()).toArray(new String[0])); 
+        message.setSubject(emailRequest.subject()); 
+        message.setText(emailRequest.message());
+        emailSender.send(message);
     }
     
 }
--- module/lib/notification/notification-email-smtp/src/test/java/ss/martin/notification/email/smtp/test/SmtpSenderTest.java
+++ module/lib/notification/notification-email-smtp/src/test/java/ss/martin/notification/email/smtp/test/SmtpSenderTest.java
@@ -1,8 +1,13 @@
 package ss.martin.notification.email.smtp.test;
 
+import java.io.File;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ss.martin.notification.email.api.EmailService;
+import ss.martin.notification.email.api.model.EmailAttachment;
+import ss.martin.notification.email.api.model.EmailContact;
+import ss.martin.notification.email.api.model.EmailRequest;
 import ss.martin.test.AbstractComponentTest;
 
 public class SmtpSenderTest extends AbstractComponentTest {
@@ -12,6 +17,16 @@ public class SmtpSenderTest extends AbstractComponentTest {
     
     @Test
     public void testSendEmail() {
-    
+        final var email = new EmailRequest(
+                new EmailContact("Site admin", "agrolavka.by@gmail.com"), 
+                new EmailContact[] { new EmailContact("Alex A", "alexandr.omelyaniuk@gmail.com") }, 
+                "Test email", 
+                "Some text for test email....",
+                new EmailAttachment[] {
+                    new EmailAttachment("test-attachment.txt", "plain/text", new File("src/test/resources/sample.txt"))
+                }
+        );
+        
+        assertDoesNotThrow(() -> emailService.sendEmail(email));
     }
 }
