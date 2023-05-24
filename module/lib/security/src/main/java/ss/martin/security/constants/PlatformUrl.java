2bbd95bfb1e07bb485e585b51bf7b0051888caef
alex <alex@sonya>
2023-05-24 20:52:41 +0300

    Registration REST controller

---
+++ module/lib/security/src/main/java/ss/martin/security/constants/PlatformUrl.java
@@ -0,0 +1,12 @@
+package ss.martin.security.constants;
+
+/**
+ * Platform URL constants.
+ * @author alex
+ */
+public final class PlatformUrl {
+    
+    private PlatformUrl() {}
+    
+    public static final String PUBLIC_RESOURCES_URL = "/api/platform/public";
+}
--- module/lib/security/src/main/java/ss/martin/security/rest/UserRegistrationRestController.java
+++ module/lib/security/src/main/java/ss/martin/security/rest/UserRegistrationRestController.java
@@ -1,27 +1,4 @@
-/*
- * The MIT License
- *
- * Copyright 2020 ss.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package ss.martin.platform.rest;
+package ss.martin.security.rest;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,20 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 import ss.martin.security.dao.UserDao;
 import ss.martin.security.model.RestResponse;
 import ss.martin.security.api.RegistrationUserService;
+import ss.martin.security.constants.PlatformUrl;
 
 /**
  * Public resources.
  * @author ss
  */
 @RestController
-@RequestMapping("/api/platform/public")
-public class PublicRESTController {
+@RequestMapping(PlatformUrl.PUBLIC_RESOURCES_URL)
+public class UserRegistrationRestController {
     /** System user service. */
     @Autowired
     private RegistrationUserService systemUserService;
     /** User DAO. */
     @Autowired
-    private UserDao userDAO;
+    private UserDao userDao;
+    
     /**
      * Finish registration.
      * @param params parameters.
@@ -60,6 +39,7 @@ public class PublicRESTController {
         systemUserService.finishRegistration(params.get("validation"), params.get("password"));
         return new RestResponse();
     }
+    
     /**
      * Check validation string.
      * @param validationString validation string.
@@ -70,7 +50,7 @@ public class PublicRESTController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Object checkValidationString(@PathVariable("validationString") String validationString)
             throws Exception {
-        final var user = userDAO.findByValidationString(validationString);
+        final var user = userDao.findByValidationString(validationString);
         return user.isEmpty() ? new RestResponse() : user.get();
     }
 }
