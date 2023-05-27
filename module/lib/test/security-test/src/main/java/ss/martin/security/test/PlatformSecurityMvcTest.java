2d2b20363354e6b73141b2ffa0dd8173b394ba6f
alex <alex@sonya>
2023-05-27 22:55:17 +0300

    JWT for test

---
+++ module/lib/test/security-test/src/main/java/ss/martin/security/test/PlatformSecurityMvcTest.java
@@ -0,0 +1,53 @@
+package ss.martin.security.test;
+
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ss.martin.core.constants.StandardRole;
+import ss.martin.core.dao.CoreDao;
+import ss.martin.security.configuration.custom.LoginResponse;
+import ss.martin.security.configuration.external.NavigationConfiguration;
+import ss.martin.security.constants.SystemUserStatus;
+import ss.martin.security.context.SecurityContext;
+import ss.martin.security.model.LoginRequest;
+import ss.martin.test.AbstractMvcTest;
+
+public abstract class PlatformSecurityMvcTest extends AbstractMvcTest {
+    
+    @Autowired
+    private NavigationConfiguration navigationConfiguration;
+    
+    @Autowired
+    protected CoreDao coreDao;
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
+    protected String jwt(final StandardRole role) {
+        final var response = callPost(
+            navigationConfiguration.login(), 
+            userRegistration(role), 
+            LoginResponse.class, 
+            HttpStatus.OK
+        );
+        return response.jwt();
+    }
+    
+    private LoginRequest userRegistration(final StandardRole role) {
+        final var email = "test.security@fake.test";
+        final var password = UUID.randomUUID().toString();
+        final var subscriptionEntity = DataFactory.generateSubscription(email);
+        final var subscription = coreDao.create(subscriptionEntity);
+        final var user = DataFactory.generateSystemUser(email, "August Rokwood");
+        user.setSubscription(subscription);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setStandardRole(role);
+        user.setStatus(SystemUserStatus.ACTIVE);
+        SecurityContext.executeBehalfUser(user, () -> {
+            coreDao.create(user);
+        });
+        return new LoginRequest(email, password);
+    }
+    
+}
