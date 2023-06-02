e27dbc3fc99eea4f25b6a77f95158bbd75d4e218
alex <alex@sonya>
2023-06-02 21:58:24 +0300

    Test profile

---
+++ module/app/agrolavka/src/test/java/ss/agrolavka/test/AgrolavkaTestApplication.java
@@ -0,0 +1,15 @@
+package ss.agrolavka.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import ss.martin.base.constants.PlatformConfiguration;
+
+@SpringBootApplication(scanBasePackages = { PlatformConfiguration.BASE_PACKAGE_SCAN, "ss.agrolavka" })
+@ConfigurationPropertiesScan({ PlatformConfiguration.BASE_PACKAGE_SCAN, "ss.agrolavka" })
+public class AgrolavkaTestApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AgrolavkaTestApplication.class, args);
+    }
+}
--- module/app/agrolavka/src/test/java/ss/agrolavka/test/PublicRestControllerTest.java
+++ module/app/agrolavka/src/test/java/ss/agrolavka/test/PublicRestControllerTest.java
@@ -2,11 +2,13 @@ package ss.agrolavka.test;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import ss.agrolavka.constants.SiteConstants;
 import ss.martin.core.constants.StandardRole;
 import ss.martin.security.test.PlatformSecurityMvcTest;
 
+@SpringBootTest(classes = AgrolavkaTestApplication.class)
 public class PublicRestControllerTest extends PlatformSecurityMvcTest {
     
     @Test
