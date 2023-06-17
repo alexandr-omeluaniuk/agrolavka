08ef6998dbcfabfa1ec7606dbc853a0165bbd709
alex <alex@sonya>
2023-06-17 22:17:38 +0300

    Mock bean

--- module/app/agrolavka/src/test/java/ss/agrolavka/test/common/AbstractAgrolavkaMvcTest.java
+++ module/app/agrolavka/src/test/java/ss/agrolavka/test/common/AbstractAgrolavkaMvcTest.java
@@ -1,9 +1,13 @@
 package ss.agrolavka.test.common;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ss.martin.security.test.PlatformSecurityMvcTest;
+import ss.martin.telegram.bot.api.TelegramBot;
 
 @SpringBootTest(classes = AgrolavkaTestApplication.class)
 public abstract class AbstractAgrolavkaMvcTest extends PlatformSecurityMvcTest {
     
+    @MockBean(name = "telegramBotOrders")
+    protected TelegramBot telegramBot;
 }
