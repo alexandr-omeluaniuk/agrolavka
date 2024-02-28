d034edca4b5452059378e009d0ecb3855c5fb47c
alex <alex@sonya>
2024-02-28 20:23:41 +0300

    Subscriptions

--- module/lib/security/src/main/java/ss/martin/security/rest/SecurityRestController.java
+++ module/lib/security/src/main/java/ss/martin/security/rest/SecurityRestController.java
@@ -3,6 +3,7 @@ package ss.martin.security.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ss.entity.martin.DataModel;
+import ss.entity.martin.Subscription;
 import ss.entity.security.SystemUser;
 import ss.martin.core.dao.CoreDao;
 import ss.martin.core.model.EntitySearchRequest;
@@ -31,7 +32,7 @@ public class SecurityRestController {
     }
 
     @GetMapping("/users")
-    public EntitySearchResponse<SystemUser> search(
+    public EntitySearchResponse<SystemUser> searchUsers(
             @RequestParam(value = "page", required = false) Integer page,
             @RequestParam(value = "page_size", required = false) Integer pageSize,
             @RequestParam(value = "order", required = false) String order,
@@ -47,17 +48,17 @@ public class SecurityRestController {
     }
 
     @GetMapping("/users/{id}")
-    public SystemUser get(@PathVariable("id") Long id) {
+    public SystemUser getUser(@PathVariable("id") Long id) {
         return coreDao.findById(id, SystemUser.class);
     }
 
     @PostMapping("/users")
-    public DataModel create(@RequestBody SystemUser user) {
+    public DataModel createUser(@RequestBody SystemUser user) {
         return systemUserService.createSubscriptionUser(user);
     }
 
     @PutMapping("/users")
-    public SystemUser update(@RequestBody SystemUser user) {
+    public SystemUser updateUser(@RequestBody SystemUser user) {
         final var fromDb = coreDao.findById(user.getId(), SystemUser.class);
         fromDb.setEmail(user.getEmail());
         fromDb.setFirstname(user.getFirstname());
@@ -66,8 +67,52 @@ public class SecurityRestController {
     }
 
     @DeleteMapping("/users/{id}")
-    public void delete(@PathVariable("id") Long id) {
+    public void deleteUser(@PathVariable("id") Long id) {
         final var fromDb = coreDao.findById(id, SystemUser.class);
+        fromDb.setActive(false);
+        coreDao.update(fromDb);
+    }
+
+    @GetMapping("/subscriptions")
+    public EntitySearchResponse<Subscription> searchSubscriptions(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "page_size", required = false) Integer pageSize,
+            @RequestParam(value = "order", required = false) String order,
+            @RequestParam(value = "order_by", required = false) String orderBy
+    ) {
+        final var request = new EntitySearchRequest(
+                page == null ? 1 : page,
+                pageSize == null ? Integer.MAX_VALUE : pageSize,
+                order,
+                orderBy
+        );
+        return coreDao.searchEntities(request, Subscription.class);
+    }
+
+    @GetMapping("/subscriptions/{id}")
+    public Subscription getSubscription(@PathVariable("id") Long id) {
+        return coreDao.findById(id, Subscription.class);
+    }
+
+    @PostMapping("/subscriptions")
+    public Subscription createSubscription(@RequestBody Subscription subscription) {
+        return systemUserService.createSubscriptionAndAdmin(subscription);
+    }
+
+    @PutMapping("/subscriptions")
+    public Subscription updateSubscription(@RequestBody Subscription subscription) {
+        final var fromDb = coreDao.findById(subscription.getId(), Subscription.class);
+        fromDb.setOrganizationName(subscription.getOrganizationName());
+        fromDb.setStarted(subscription.getStarted());
+        fromDb.setExpirationDate(subscription.getExpirationDate());
+        fromDb.setSubscriptionAdminEmail(subscription.getSubscriptionAdminEmail());
+        fromDb.setActive(subscription.isActive());
+        return coreDao.update(fromDb);
+    }
+
+    @DeleteMapping("/subscriptions/{id}")
+    public void deleteSubscription(@PathVariable("id") Long id) {
+        final var fromDb = coreDao.findById(id, Subscription.class);
         fromDb.setActive(false);
         coreDao.update(fromDb);
     }
