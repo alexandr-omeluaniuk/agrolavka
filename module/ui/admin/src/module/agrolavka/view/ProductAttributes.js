0e15b220d2efd6d4354036c32cc9e4f7cd49d282
alex <alex@sonya>
2024-05-20 22:08:02 +0300

    Product attribute item UI

--- module/ui/admin/src/module/agrolavka/view/ProductAttributes.js
+++ module/ui/admin/src/module/agrolavka/view/ProductAttributes.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 import { TableConfig, TableColumn, FormConfig, FormField, ALIGN_RIGHT, ApiURL, Validator } from '../../../util/model/TableConfig';
 import { TYPES, VALIDATORS } from '../../../service/DataTypeService';
@@ -11,11 +12,23 @@ import DataTable from '../../../component/datatable/DataTable';
 import AppURLs from '../../../conf/app-urls';
 import { NavLink } from "react-router-dom";
 import moment from 'moment';
+import { Icon, IconButton, Tooltip } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    addItem: {
+        color: theme.palette.success.main
+    }
+}));
 
 function ProductAttributes() {
     const { t } = useTranslation();
     const [tableConfig, setTableConfig] = React.useState(null);
+    const classes = useStyles();
     // ------------------------------------------------------- METHODS --------------------------------------------------------------------
+    const addItem = () => {
+
+    };
+
     const updateTable = () => {
         const apiUrl = new ApiURL(
                 '/agrolavka/protected/product-attributes',
@@ -38,7 +51,14 @@ function ProductAttributes() {
             }),
             new TableColumn('created_date', t('m_agrolavka:attributes.created'), (row) => {
                 return moment(row.created).locale('ru').format('DD.MM.yyyy HH:mm');
-            }).width('160px').alignment(ALIGN_RIGHT)
+            }).width('160px').alignment(ALIGN_RIGHT),
+            new TableColumn('btn_add_item', '', (row) => {
+                return <Tooltip title={t('m_agrolavka:attributes.addItem')}>
+                            <IconButton onClick={() => addItem(row)}>
+                                <Icon className={classes.addItem}>post_add</Icon>
+                            </IconButton>
+                        </Tooltip>;
+            }).width('40px').alignment(ALIGN_RIGHT).asAction()
         ], new FormConfig([
             new FormField('id', TYPES.ID).hide(),
             new FormField('name', TYPES.TEXTFIELD, t('m_agrolavka:attributes.name')).setGrid({xs: 12, md: 12}).validation([
