a1d3eb46e673295ddaccb537dfe7049b1dd2dedd
alex <alex@sonya>
2024-05-20 21:16:56 +0300

    Product attribute

---
+++ module/app/agrolavka/src/main/java/ss/entity/agrolavka/ProductAttributeItem.java
@@ -0,0 +1,58 @@
+package ss.entity.agrolavka;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import ss.entity.martin.DataModel;
+
+@Entity
+@Table(name = "product_attribute_item")
+public class ProductAttributeItem extends DataModel {
+
+    @NotNull
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @NotNull
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_attribute_id")
+    private ProductAttribute productAttribute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ProductAttribute getProductAttribute() {
+        return productAttribute;
+    }
+
+    public void setProductAttribute(ProductAttribute productAttribute) {
+        this.productAttribute = productAttribute;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (getId() != null ? getId().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof ProductAttributeItem other)) {
+            return false;
+        }
+        return !((this.getId() == null && other.getId() != null)
+            || (this.getId() != null && !this.getId().equals(other.getId())));
+    }
+
+    @Override
+    public String toString() {
+        return "ss.entity.agrolavka.ProductAttributeItem[ id=" + getId() + " ]";
+    }
+}
