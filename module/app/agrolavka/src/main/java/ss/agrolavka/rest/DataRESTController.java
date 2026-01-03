0646a834dc52d451f0a374276c8c2faf32c78b38
alex <alex@sonya>
2026-01-03 19:22:32 +0300

    Backup service

--- module/app/agrolavka/src/main/java/ss/agrolavka/rest/DataRESTController.java
+++ module/app/agrolavka/src/main/java/ss/agrolavka/rest/DataRESTController.java
@@ -5,20 +5,19 @@
  */
 package ss.agrolavka.rest;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ss.agrolavka.service.BackupService;
 import ss.agrolavka.task.DataUpdater;
+import ss.martin.security.api.AlertService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * My Sklad actions.
@@ -27,6 +26,9 @@ import ss.agrolavka.task.DataUpdater;
 @RestController
 @RequestMapping("/api/agrolavka/protected/mysklad")
 public class DataRESTController {
+
+    @Autowired
+    private AlertService alertService;
     /** Data updater. */
     @Autowired
     private DataUpdater dataUpdater;
@@ -48,12 +50,14 @@ public class DataRESTController {
      * @param response HTTP response.
      * @throws Exception error. 
      */
-    @RequestMapping(value = "/backup", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @PutMapping("/backup")
     public void backup(HttpServletResponse response) throws Exception {
-        File backup = backupService.doBackup();
-        response.getOutputStream().write(Files.readAllBytes(Paths.get(backup.toURI())));
-        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + backup.getName());
-        response.addHeader("ContentType", "application/zip");
-        response.addHeader("Content-Length", backup.length() + "");
+        new Thread(() -> {
+            try {
+                backupService.createBackup();
+            } catch (Exception e) {
+                alertService.sendAlert("Create backup failed", e);
+            }
+        }).start();
     }
 }
--- module/app/agrolavka/src/main/java/ss/agrolavka/service/BackupService.java
+++ module/app/agrolavka/src/main/java/ss/agrolavka/service/BackupService.java
@@ -16,19 +16,182 @@
  */
 package ss.agrolavka.service;
 
-import java.io.File;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.FileList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ss.martin.images.storage.configuration.external.StorageConfiguration;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
- * Backup data service.
+ * Backup service implementation.
  * @author alex
  */
-public interface BackupService {
-    /**
-     * Do data backup.
-     * @return backup file.
-     * @throws Exception error.
-     */
-    File doBackup() throws Exception;
+@Service
+public class BackupService {
+    /** Logger. */
+    private static final Logger LOG = LoggerFactory.getLogger(BackupService.class);
+    /** Backup directory. */
+    private static final File BACKUP_DIR = new File("/tmp/agrolavka");
+    /** DB username. */
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+    /** DB password. */
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+    /** Platform configuration. */
+    @Autowired
+    private StorageConfiguration storageConfiguration;
 
-    void createBackup() throws Exception;
+    @Value("${gdrive.pups}")
+    private String gdrivePups;
+    @Value("${gdrive.store}")
+    private String gdriveStore;
+    @Value("${gdrive.aname}")
+    private String gdriveAname;
+
+
+    public void createBackup() throws Exception {
+        if (!BACKUP_DIR.exists()) {
+            BACKUP_DIR.mkdirs();
+        }
+        final var service = getInstance();
+        File dumpFile = mysqlDump();
+        LOG.info("SQL Dump file path: " + dumpFile.getAbsolutePath());
+        File imagesZip = imagesBackup();
+        LOG.info("Images zip file path: " + imagesZip.getAbsolutePath());
+        File backupFile = backupArchive();
+        LOG.info("Backup archive file path: " + backupFile.getAbsolutePath());
+        com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+        fileMetadata.setName(new SimpleDateFormat("dd.MM.yyyy").format(new Date()) + "-backup.zip");
+        fileMetadata.setCreatedTime(new DateTime(System.currentTimeMillis()));
+        service.files().create(
+            fileMetadata,
+            new InputStreamContent("application/zip", new FileInputStream(backupFile))
+        ).setFields("id").execute();
+        LOG.info("Backup uploaded");
+        // delete outdated backups
+        final var limitDate = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(30);
+        FileList result = service.files().list()
+            .setPageSize(100).setFields("files(id, name, createdTime)")
+            .execute();
+        final var files = result.getFiles();
+        for (final var f : files) {
+            LOG.info("Backup file found {}", f.getName());
+            if (f.getCreatedTime() != null && f.getName().contains("backup.zip") && f.getCreatedTime().getValue() < limitDate) {
+                service.files().delete(f.getId()).execute();
+                LOG.info("Outdated backup file deleted: {}", f.getName());
+            }
+        }
+    }
+
+    private File backupArchive() throws Exception {
+        File backupFile = new File("/tmp", "agrolavka_backup.zip");
+        try (FileOutputStream fos = new FileOutputStream(backupFile); ZipOutputStream zos = new ZipOutputStream(fos)) {
+            addDirToZipArchive(zos, BACKUP_DIR, null);
+            zos.flush();
+            fos.flush();
+        }
+        return backupFile;
+    }
+    
+    private File imagesBackup() throws Exception {
+        File archive = new File(BACKUP_DIR, "agrolavka_images.zip");
+        try (FileOutputStream fos = new FileOutputStream(archive); ZipOutputStream zos = new ZipOutputStream(fos)) {
+            addDirToZipArchive(zos, new File(storageConfiguration.path()), null);
+            zos.flush();
+            fos.flush();
+        }
+        return archive;
+    }
+    
+    private File mysqlDump() throws Exception {
+        File dumpFile = new File(BACKUP_DIR, "agrolavka.sql");
+        String command = "mysqldump -u" + dbUser + " -p" + dbPassword + " agrolavka -r " + dumpFile.getAbsolutePath();
+        LOG.debug("Command: " + command);
+        Process process = Runtime.getRuntime().exec(command);
+        int code = process.waitFor();
+        LOG.info("mysqldump code: " + code);
+        if (code != 0) {
+            byte[] errData = process.getErrorStream().readAllBytes();
+            LOG.info("error: " + new String(errData, "UTF-8"));
+        }
+        return dumpFile;
+    }
+    
+    public static void addDirToZipArchive(ZipOutputStream zos, File fileToZip, String parrentDirectoryName)
+            throws Exception {
+        if (fileToZip == null || !fileToZip.exists()) {
+            return;
+        }
+        String zipEntryName = fileToZip.getName();
+        if (parrentDirectoryName != null && !parrentDirectoryName.isEmpty()) {
+            zipEntryName = parrentDirectoryName + "/" + fileToZip.getName();
+        }
+        if (fileToZip.isDirectory()) {
+            LOG.trace("+" + zipEntryName);
+            for (File file : fileToZip.listFiles()) {
+                addDirToZipArchive(zos, file, zipEntryName);
+            }
+        } else {
+            LOG.trace("   " + zipEntryName);
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = new FileInputStream(fileToZip);
+            zos.putNextEntry(new ZipEntry(zipEntryName));
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                zos.write(buffer, 0, length);
+            }
+            zos.closeEntry();
+            fis.close();
+        }
+    }
+
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+        throws IOException {
+        final var jsonFactory = GsonFactory.getDefaultInstance();
+        InputStream in = new FileInputStream(gdrivePups);
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(jsonFactory, new InputStreamReader(in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+            HTTP_TRANSPORT, jsonFactory, clientSecrets, Collections.singletonList(DriveScopes.DRIVE_FILE))
+            .setDataStoreFactory(new FileDataStoreFactory(new File(gdriveStore)))
+            .setAccessType("offline")
+            .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        receiver.stop();
+        return credential;
+    }
+
+    private Drive getInstance() throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, GsonFactory.getDefaultInstance(), getCredentials(HTTP_TRANSPORT))
+            .setApplicationName(gdriveAname)
+            .build();
+        return service;
+    }
 }
